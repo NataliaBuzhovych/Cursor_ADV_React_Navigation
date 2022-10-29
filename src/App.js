2687e7ddea21e1f0fb97bad20946cca8cf92fc84
Natalia Buzhovych <tuzhay@gmail.com>
2022-10-29 15:01:13 +0300

    hw-18

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import "./App.css";
 import { render } from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 import HomePage from "./components/HomePage";
 import AboutPage from "./components/AboutPage";
 import UsersPage from "./components/UsersPage";
 import UserPage from "./components/Userpage";
 import Photes from "./components/Photes";
+import { NoMatch } from "./components/NoMatch";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route path="*" element={<HomePage />} />
           <Route path="/phote" element={<Photes />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/users" element={<UsersPage />} />
-          <Route path="users/:id" element={<UserPage />} />
+          <Route path="/users/:id" element={<UserPage />} />
         </Routes>
       </BrowserRouter>
     </div>
